17f5cf619556a3f2e5dfda398102448212be3560
shadow-developper <axel.barth@yahoo.com>
2025-07-01 10:15:14 +0200

    Multer complexUploaderMiddleware - Épisode n°41

---
+++ middlewares/complexUploader.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const { StatusCodes } = require("http-status-codes");
+
+const multipleFileUploaderMiddleware = (req, res, next) => {
+    const uploader = req.app.locals.uploader;
+
+    const multipleFileUploaderMiddleware = uploader.array("images, 10"); //10 Images max
+
+    multipleFileUploader(req, res, (error) => {
+        if (error instanceof multer.MulterError) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+        } else if (error) {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
+        }
+        next();
+    });
+};
+
+module.exports = multipleFileUploaderMiddleware;
