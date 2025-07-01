fafe84dd65b86c55b54800510b8ccf2a6e28e2cf
shadow-developper <axel.barth@yahoo.com>
2025-07-01 15:28:55 +0200

    Login endpoint - Épisode n°49

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -35,7 +35,40 @@ const register = async (req,res) => {
 };
 
 const login = async (req,res) => {
-    
+    try {
+        const { email, password } = req.body;
+        // On regarde s'il y a des champs manquants
+        if(!email || !password){
+            return res.status(StatusCodes.BAD_REQUEST).send("Missing field(s)");
+        }
+        // Vérification que l'utilisateur existe bien
+        const foundUser = await User.findOne( { email });
+        if(!foundUser){
+            return res.status(StatusCodes.UNAUTHORIZED).send("Invalid credentials");
+        }
+        // Vérification du mot de passe
+        const isMatch = await bcrypt.compare(password, foundUser.password);
+        if(!isMatch){
+            return res.status(StatusCodes.UNAUTHORIZED).send("Invalid credentials");
+        }
+        // Tout va bien, nous envoyons donc les données sous forme de jeton à l'utilisateur
+        const jwtSecretKey = process.env.JWT_SECRET_KEY;
+        const token = jwt.sign(
+            {
+                id: foundUser._id,
+                username: foundUser.username,
+                email: foundUser.email
+            },
+            jwtSecretKey, 
+            { expiresIn: "1h"} // Le token expire dans 1 heure
+        );
+        // Strip l'utilisateur trouvé de son mot de passe et de la version interne
+        const { password: _, __v, ...userWithoutSensitiveData } = foundUser._doc;
+        return res.status(StatusCodes.OK).send({ user: userWithoutSensitiveData, token});
+    } catch (error) {
+        console.log(`Error in user login : ${error}`);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("An error occurred during login");
+    }
 };
 
 module.exports = { register, login };
