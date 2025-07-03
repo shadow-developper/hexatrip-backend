0c2d3e717cc243ef97ad1507e0d7df72c96c2a1c
shadow-developper <axel.barth@yahoo.com>
2025-07-03 10:25:02 +0200

    Stripe endpoint suite - Épisode n°61

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -7,18 +7,23 @@ const Stripe = require("stripe");
 const createStripeSession = async (req,res) => {
     try {
         // Initialiser la session Stripe
+        const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY);
 
         // Importer tout ce qui a été envoyé par le client
-
+        const order = req.body.order;
+        const items = req.body.items;
+        const token = req.body.token;
+        
         // Récupérer le voyage vendu dans la transaction à partir de la base de données items est un tableau d'un seul élément : items[0] qui est le voyage
-
+        const foundTrip = await Trip.findById(items[0]).id
         // Faire la transaction via Stripe
 
         // Pendant que le bon de commande est dans la base de données (en mode « non connecté/visiteur » et en mode connecté)
 
         // Sortir
     } catch (error) {
-        
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message});
     }
 };
 
